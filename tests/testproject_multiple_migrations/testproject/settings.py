3ccabfac0b5f5b379febd4226bd3f149e4f327f9
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2023-03-07 00:23:05 +0400

    Fix the DB path in Django<3.1

--- tests/testproject_multiple_migrations/testproject/settings.py
+++ tests/testproject_multiple_migrations/testproject/settings.py
@@ -75,7 +75,10 @@ WSGI_APPLICATION = "testproject.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
+    }
 }
 
 
